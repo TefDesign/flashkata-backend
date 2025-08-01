4ee2d41a535d2cf9ea20fd8923fb5f906e4a8a1a
canadas hugo <canadashcb@gmail.com>
2025-08-01 17:18:09 +0200

    feature initialisation backend

---
+++ app.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+require('./models/connection')
+
+app.use(express.json());
+
+// liste des routes
+const indexRouter = require('./route/index');
+
+
+
+// redir des routes
+app.use('/', indexRouter);
+
+
+
+
+app.listen(port, () => {
+    console.log(`KATAAA lancé sur http://localhost:${port}`);
+});
+
---
+++ models/connection.js
@@ -0,0 +1,7 @@
+
+  const mongoose = require('mongoose');
+  const connectionString = process.env.CONNECTION_STRING
+  mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+    .then(() => console.log(chalk.green('KATA Database connected')))
+    .catch(error => console.error(error));
+  // ne pas oublier de require le fichier dans app
