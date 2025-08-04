25e896a2b1a78ceb6c4f052c85eeeb81c1a646c9
canadas hugo <canadashcb@gmail.com>
2025-08-04 14:32:21 +0200

    gen kata aleatoire

--- route/index.js
+++ route/index.js
@@ -2,19 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Katakana = require('../models/Katakana');
 const Hiragana = require('../models/Hiragana');
+const uid2 = require("uid2");
 
+router.post('/kataGen', async (req, res) => {
+  for (let i = 0; i < 100; i++) {
+    const kata = await new Katakana({
+    name: uid2(10),
+    image: 'req.body.image',
+    placeholder: 'req.body.placeholder',
+    sound: 'req.body.sound'
+  }).save()
+    const hira = await new Hiragana({
+    name: uid2(10),
+    image: 'req.body.image',
+    placeholder: 'req.body.placeholder',
+    sound: 'req.body.sound'
+  }).save()
+  res.json({result : true})
+  }
+})
 
 router.get('/', (req, res) => {
   res.json('kata!!!!');
 });
 
 router.get('/katakana', async(req, res) => {
-  const resp = await Katakana({})
+  const resp = await Katakana.find({})
   res.json(resp)
 })
 
 router.get('/hiragana', async(req, res) => {
-  const resp = await Hiragana({})
+  const resp = await Hiragana.find({})
   res.json(resp)
 })
 
