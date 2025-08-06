435bd9ed7b787bac0a45de22227f8dbf65f860e2
Antonin <cosson.antonin@gmail.com>
2025-08-06 17:30:17 +0200

    test filtered by unviewed

--- route/afficherExos.js
+++ route/afficherExos.js
@@ -9,7 +9,14 @@ const KatakanaProgress = require("../models/KatakanaProgress");
 const HiraganaProgress = require("../models/HiraganaProgress");
 const { checkBody } = require("../modules/checkBody");
 
-router.post("/exosNewKataORHira", async (req, res) =>{
+
+
+
+
+
+router.post("/UnviewedCard", async (req, res) =>{
+
+
 
     // ajout token plus tard
     if (!checkBody(req.body, ["id", "password"])) {
@@ -17,26 +24,84 @@ router.post("/exosNewKataORHira", async (req, res) =>{
         return;
     }
 
-    const nbKata = Number(req.body.katakana)
-    const nbHira = Number(req.body.hiragana)
-    const nbViewsKata = Number(req.body.nbViews)
+    const nbKata = Number(req.body.NbByCursorKata) // Choix du nb de kata par le curseur
+    const nbHira = Number(req.body.NbByCursorHira) // Nb
+    
+    const NewBatchKata = req.body.needNewBatchKata // Nouveau lot de cartes?
+    const NewBatchHira = req.body.needNewBatchHira // Booléen
+    
+    const onlyViewedKata = req.body.onlyViewedKata // Cartes déjà vu?
+    const onlyViewedHira = req.body.onlyViewedHira // Booléen
 
-    console.log("la:", nbViewsKata)
+
+    if (req.body.NbByCursorKata && onlyViewedKata === "false" && !isNaN(nbKata)) {
+        try {
+            let user = await User.findById({_id: req.body.id})
+            .populate("katakanaProgress")
+
+            let kataAll = await Katakana.find();
+            let kataProgList = user.katakanaProgress;
+
+            // fusion kataAll et kataProgress par un map
+            let kataAllWithProgress = kataAll.map(kata => {
+                // p = 1 kataProgress de user
+                let prog = kataProgList.find(p => p.katakanaId.toString() === kata._id.toString());
+                return {
+                    ...kata.toObject(),
+                    progression: prog || null
+                };
+            })
+            console.log("step3 good")
+            console.log("progression", kataAllWithProgress[0].progression.nbViews)
+
+            // let theseAreNotViewed = []
+            let filtered = []
+
+            const KataProgressByUnviewed = kataAllWithProgress.filter(e =>  { return e.progression.nbViews <= 0})
+            filtered.push(...KataProgressByUnviewed)
+
+            console.log("step4 good")
+            console.log("filtered", filtered.length)
+
+            let selected = [];
+                // 
+            for (let i = 0  ; i < nbKata && filtered.length > 0 ; i++){
+
+                const randomIndex = Math.floor(Math.random() * filtered.length);
+
+                selected.push(filtered[randomIndex])
+                filtered.splice(randomIndex, 1)
+            }
+
+            console.log("step5 good")
+            console.log("selected", selected.length)
+
+            return res.json(selected)
+        } catch(error) {
+            return res.json(error)
+        }
+    }
 
 })
 
-router.post("/exosAllKataORHira", async (req, res) => {
+router.post("/OnlyViewedCard", async (req, res) => {
 
                             // ajout token plus tard
     if (!checkBody(req.body, ["id", "password"])) {
         res.json({ result: false, error: "Missing or empty fields" });
         return;
     }
-            // changer nom plus explicite que req.body.katakana genre req.body.NbCurseur? Des avis?
-    const nbKata = Number(req.body.katakana)
-    const nbHira = Number(req.body.hiragana)
 
-    if (req.body.katakana && !isNaN(nbKata)) {
+    const nbKata = Number(req.body.NbByCursorKata)
+    const nbHira = Number(req.body.NbByCursorHira)
+    
+    const NewBatchKata = req.body.needNewBatchKata
+    const NewBatchHira = req.body.needNewBatchHira
+    
+    const onlyViewedKata = req.body.onlyViewedKata
+    const onlyViewedHira = req.body.onlyViewedHira
+
+    if (req.body.NbByCursorKata && !isNaN(nbKata)) {
         try {
             let user = await User.findById({_id: req.body.id})
             .populate("katakanaProgress")
@@ -70,7 +135,7 @@ router.post("/exosAllKataORHira", async (req, res) => {
             return res.json(error)
         }}
 
-    else if (req.body.hiragana) {
+    else if (req.body.NbByCursorHira && !isNaN(nbHira)) {
         try {
 
             let user = await User.findById({_id: req.body.id})
@@ -106,7 +171,7 @@ router.post("/exosAllKataORHira", async (req, res) => {
 
 })
 
-router.post("/exosAllKataANDHira", async (req, res) =>{
+router.post("/Viewed&Unviewed", async (req, res) =>{
     
 })
 
