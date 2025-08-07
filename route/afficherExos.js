ff783ed59d3b0c960e2ecea8adb7ceefbfe23da4
Antonin <cosson.antonin@gmail.com>
2025-08-07 16:17:26 +0200

    ttt

--- route/afficherExos.js
+++ route/afficherExos.js
@@ -9,6 +9,10 @@ const KatakanaProgress = require("../models/KatakanaProgress");
 const HiraganaProgress = require("../models/HiraganaProgress");
 const { checkBody } = require("../modules/checkBody");
 
+const { switcherType } = require("../modules/switcherType")
+
+
+
 
 router.post("/UnviewedCard", async (req, res) =>{
 
@@ -18,46 +22,89 @@ if (!checkBody(req.body, ["id", "password"])) {
     return;
 }
 
-    const nbKata = Number(req.body.NbByCursorKata) // Choix du nb de kata par le curseur
-    const nbHira = Number(req.body.NbByCursorHira) // Nb
-    
-    const NewBatchKata = req.body.needNewBatchKata // Nouveau lot de cartes?
-    const NewBatchHira = req.body.needNewBatchHira // Booléen
+    const nbCursor = Number(req.body.NbByCursor) // Choix du nb de kata par le curseur
+
+    const NewBatch = req.body.needNewBatch // Booléen
     
     const onlyViewedKata = req.body.onlyViewedKata // Cartes déjà vu?
     const onlyViewedHira = req.body.onlyViewedHira // Booléen
 
 // Unviewed Kata Ok
-if (req.body.NbByCursorKata && onlyViewedKata === "false" && !isNaN(nbKata) && NewBatchKata) {
+// req.body.NbByCursorKata && onlyViewedKata === "false" && !isNaN(nbCursor)
+if (true) {
         try {
+            
+            const type = req.body.type
 
             // Chargé User et populate le progress dans User
             let user = await User.findById({_id: req.body.id})
-            .populate("katakanaProgress")
+            type === "hiragana" || type === "All" ? await user.populate("hiraganaProgress") : null
+            type === "katakana" || type === "All" ? await user.populate("katakanaProgress") : null
 
-
+console.log("t1", user)
             // 
-            let kataAll = await Katakana.find();
-            let kataProgList = user.katakanaProgress;
+            let kataAll = [];
+            
+            if (type === "katakana" || type === "All"){
+                const kata = await Katakana.find()
+                kataAll = [...kataAll, ...kata]
+            }
+            
+            if (type === "hiragana" || type === "All"){
+                const kata = await Hiragana.find()
+                kataAll = [...kataAll, ...kata]
+            }
+       
+// console.log("ggg", kataAll.length)
+
+            let kataProgList = [];
+
+            if (type === "katakana" || type === "All"){
+                kataProgList = [...kataProgList, ...user.katakanaProgress]
+            }
+            
+            if (type === "hiragana" || type === "All"){
+                kataProgList = [...kataProgList, ...user.hiraganaProgress]
+            }
+
+console.log("kaaaa", kataProgList.length)
+        
 
             // fusion kataAll et kataProgress par un map
+
             let kataAllWithProgress = kataAll.map(kata => {
-                // p = 1 kataProgress de user
-                let prog = kataProgList.find(p => p.katakanaId.toString() === kata._id.toString());
+
+                console.log("l")
+
+                let prog;
+                console.log("pr", kataProgList.some(p => p.katakanaId === kata._id))
+
+                if (kataProgList.some(p => p.katakanaId === kata._id)){
+                    console.log("untruc")
+                    prog = kataProgList.find(p => p.katakanaId.toString() === kata._id.toString()); 
+                }
+                
+                if (kataProgList.some(p => p.hiraganaId.toString() === kata._id.toString())){
+                    console.log("unautretruc")
+                    prog = kataProgList.find(p => p.hiraganaId.toString() === kata._id.toString());
+                }
+
+
                 return {
                     ...kata.toObject(),
                     progression: prog || null
                 };
             })
 
-// console.log("step3 good")
+console.log("step3 good")
+
 // console.log("progression", kataAllWithProgress[0].progression.nbViews)
 
 
             let moy = kataAllWithProgress.reduce((acc, value) => value.progression.nbViews + acc,0)/kataAllWithProgress.length
 
             let filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews <= 0})
-            filtered && filtered.length > nbKata ? null : filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews < moy})
+            filtered && filtered.length > nbCursor ? null : filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews < moy})
 
 
 console.log("step4 good")
@@ -68,7 +115,7 @@ console.log("moy", moy)
             //  
             let selected = [];
 
-            for (let i = 0 ; i < nbKata; i++){
+            for (let i = 0 ; i < nbCursor; i++){
 
                 if(filtered.length > 0){
 
@@ -87,7 +134,7 @@ console.log("moy", moy)
             }
 
 
-// console.log("step5 good")
+console.log("step5 good")
 // console.log("selected", selected.length)
 
             return res.json(selected)
@@ -99,7 +146,7 @@ console.log("moy", moy)
 }
 
 // Unviewed Hira Ok
-if (req.body.NbByCursorHira && onlyViewedHira === "false" && !isNaN(nbHira) && NewBatchHira) {
+if (req.body.NbByCursorHira && onlyViewedHira === "false" && !isNaN(nbCursor)) {
     try {
 
         // Chargé User et populate le progress dans User
@@ -168,6 +215,7 @@ console.log("moy", moy)
         return res.json(error)
     }
 }
+
 })
 
 router.post("/OnlyViewedCard", async (req, res) => {
