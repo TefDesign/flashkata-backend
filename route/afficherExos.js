7a58aefc950694c2715ebb70e706ef35dafa22bf
Antonin <cosson.antonin@gmail.com>
2025-08-07 14:40:25 +0200

    Routes a factorisé pour exos

--- route/afficherExos.js
+++ route/afficherExos.js
@@ -12,11 +12,11 @@ const { checkBody } = require("../modules/checkBody");
 
 router.post("/UnviewedCard", async (req, res) =>{
 
-         // ajout token plus tard
-    if (!checkBody(req.body, ["id", "password"])) {
-        res.json({ result: false, error: "Missing or empty fields" });
-        return;
-    }
+     // ajout token plus tard
+if (!checkBody(req.body, ["id", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+}
 
     const nbKata = Number(req.body.NbByCursorKata) // Choix du nb de kata par le curseur
     const nbHira = Number(req.body.NbByCursorHira) // Nb
@@ -27,8 +27,8 @@ router.post("/UnviewedCard", async (req, res) =>{
     const onlyViewedKata = req.body.onlyViewedKata // Cartes déjà vu?
     const onlyViewedHira = req.body.onlyViewedHira // Booléen
 
-// If unviewed Kata ok
-    if (req.body.NbByCursorKata && onlyViewedKata === "false" && !isNaN(nbKata)) {
+// Unviewed Kata Ok
+if (req.body.NbByCursorKata && onlyViewedKata === "false" && !isNaN(nbKata) && NewBatchKata) {
         try {
 
             // Chargé User et populate le progress dans User
@@ -50,16 +50,178 @@ router.post("/UnviewedCard", async (req, res) =>{
                 };
             })
 
-console.log("step3 good")
-console.log("progression", kataAllWithProgress[0].progression.nbViews)
+// console.log("step3 good")
+// console.log("progression", kataAllWithProgress[0].progression.nbViews)
 
 
-            let filtered = []
+            let moy = kataAllWithProgress.reduce((acc, value) => value.progression.nbViews + acc,0)/kataAllWithProgress.length
 
-            const KataProgressByUnviewed = kataAllWithProgress.filter(e =>  { return e.progression.nbViews <= 0})
-            filtered.push(...KataProgressByUnviewed)
+            let filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews <= 0})
+            filtered && filtered.length > nbKata ? null : filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews < moy})
 
 
+console.log("step4 good")
+console.log("filtered", filtered.length)
+console.log("moy", moy)
+
+
+            //  
+            let selected = [];
+
+            for (let i = 0 ; i < nbKata; i++){
+
+                if(filtered.length > 0){
+
+                    const randomIndex = Math.floor(Math.random() * filtered.length);
+
+                    selected.push(filtered[randomIndex])
+                    filtered.splice(randomIndex, 1)
+                } else {
+
+                    const randomIndex = Math.floor(Math.random() * kataAllWithProgress.length);
+
+                    selected.push(kataAllWithProgress[randomIndex])
+                    kataAllWithProgress.splice(randomIndex, 1)
+                }
+                
+            }
+
+
+// console.log("step5 good")
+// console.log("selected", selected.length)
+
+            return res.json(selected)
+
+        } catch(error) {
+            
+            return res.json(error)
+        }
+}
+
+// Unviewed Hira Ok
+if (req.body.NbByCursorHira && onlyViewedHira === "false" && !isNaN(nbHira) && NewBatchHira) {
+    try {
+
+        // Chargé User et populate le progress dans User
+        let user = await User.findById({_id: req.body.id})
+        .populate("hiraganaProgress")
+
+
+        // 
+        let hiraAll = await Hiragana.find();
+        let hiraProgList = user.hiraganaProgress;
+
+        // fusion hiraAll et hiraProgress par un map
+        let hiraAllWithProgress = hiraAll.map(hira => {
+            // p = 1 hiraProgress de user
+            let prog = hiraProgList.find(p => p.hiraganaId.toString() === hira._id.toString());
+            return {
+                ...hira.toObject(),
+                progression: prog || null
+            };
+        })
+
+// console.log("step3 good")
+// console.log("progression", hiraAllWithProgress[0].progression.nbViews)
+
+
+        let moy = hiraAllWithProgress.reduce((acc, value) => value.progression.nbViews + acc,0)/hiraAllWithProgress.length
+
+        let filtered = hiraAllWithProgress.filter(e =>  { return e.progression.nbViews <= 0})
+        filtered && filtered.length > nbHira ? null : filtered = hiraAllWithProgress.filter(e =>  { return e.progression.nbViews < moy})
+
+
+console.log("step4 good")
+console.log("filtered", filtered.length)
+console.log("moy", moy)
+
+
+        //  
+        let selected = [];
+
+        for (let i = 0 ; i < nbHira; i++){
+
+            if(filtered.length > 0){
+
+                const randomIndex = Math.floor(Math.random() * filtered.length);
+
+                selected.push(filtered[randomIndex])
+                filtered.splice(randomIndex, 1)
+            } else {
+
+                const randomIndex = Math.floor(Math.random() * hiraAllWithProgress.length);
+
+                selected.push(hiraAllWithProgress[randomIndex])
+                hiraAllWithProgress.splice(randomIndex, 1)
+            }
+            
+        }
+
+
+// console.log("step5 good")
+// console.log("selected", selected.length)
+
+        return res.json(selected)
+
+    } catch(error) {
+        
+        return res.json(error)
+    }
+}
+})
+
+router.post("/OnlyViewedCard", async (req, res) => {
+
+ // ajout token plus tard
+ if (!checkBody(req.body, ["id", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+}
+    
+    const nbKata = Number(req.body.NbByCursorKata) // Choix du nb de kata par le curseur
+    const nbHira = Number(req.body.NbByCursorHira) // Nb
+    
+    const NewBatchKata = req.body.needNewBatchKata // Nouveau lot de cartes?
+    const NewBatchHira = req.body.needNewBatchHira // Booléen
+    
+    const onlyViewedKata = req.body.onlyViewedKata // Cartes déjà vu?
+    const onlyViewedHira = req.body.onlyViewedHira // Booléen
+
+    
+// Viewed Kata Ok
+    if (req.body.NbByCursorKata && onlyViewedKata === "true" && !isNaN(nbKata) && NewBatchKata) {
+        try {
+
+            // Chargé User et populate le progress dans User
+            let user = await User.findById({_id: req.body.id})
+            .populate("katakanaProgress")
+
+
+            // 
+            let kataAll = await Katakana.find();
+            let kataProgList = user.katakanaProgress;
+
+            // fusion kataAll et kataProgress par un map
+            let kataAllWithProgress = kataAll.map(kata => {
+                // p = 1 kataProgress de user
+                let prog = kataProgList.find(p => p.katakanaId.toString() === kata._id.toString());
+                return {
+                    ...kata.toObject(),
+                    progression: prog || null
+                };
+            })
+
+// console.log("step3 good")
+// console.log("progression", kataAllWithProgress[0].progression.nbViews)
+
+            let moy = kataAllWithProgress.reduce((acc, value) => value.progression.nbViews + acc,0)/kataAllWithProgress.length
+
+            let filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews > 0})
+            filtered && filtered.length > nbKata ? null : filtered = kataAllWithProgress.filter(e =>  { return e.progression.nbViews >= moy})
+
+
+// console.log(kataAllWithProgress)
+// console.log(moy)
 console.log("step4 good")
 console.log("filtered", filtered.length)
 
@@ -67,12 +229,21 @@ console.log("filtered", filtered.length)
             //  
             let selected = [];
 
-            for (let i = 0 ; i < nbKata && filtered.length > 0; i++){
+            for (let i = 0 ; i < nbKata; i++){
 
-                const randomIndex = Math.floor(Math.random() * filtered.length);
+                if(filtered.length > 0){
 
-                selected.push(filtered[randomIndex])
-                filtered.splice(randomIndex, 1)
+                    const randomIndex = Math.floor(Math.random() * filtered.length);
+
+                    selected.push(filtered[randomIndex])
+                    filtered.splice(randomIndex, 1)
+                } else {
+
+                    const randomIndex = Math.floor(Math.random() * kataAllWithProgress.length);
+
+                    selected.push(kataAllWithProgress[randomIndex])
+                    kataAllWithProgress.splice(randomIndex, 1)
+                }
             }
 
 
@@ -83,22 +254,85 @@ console.log("selected", selected.length)
 
         } catch(error) {
             
-            return res.json(error)
+            return res.json({error: error, result: false})
         }
     }
 
-// Else If unviewed Hira not ok
-    else if(req.body.onlyViewedHira && onlyViewedHira === "false" && !isNaN(nbHira)){
+// Viewed Hira
+    else if(req.body.NbByCursorHira && onlyViewedHira === "true" && !isNaN(nbHira)&& NewBatchHira){
+        try {
 
+            // Chargé User et populate le progress dans User
+            let user = await User.findById({_id: req.body.id})
+            .populate("hiraganaProgress")
+
+
+            // 
+            let hiraAll = await Hiragana.find();
+            let hiraProgList = user.hiraganaProgress;
+
+            // fusion hiraAll et hiraProgress par un map
+            let hiraAllWithProgress = hiraAll.map(hira => {
+                // p = 1 hiraProgress de user
+                let prog = hiraProgList.find(p => p.hiraganaId.toString() === hira._id.toString());
+                return {
+                    ...hira.toObject(),
+                    progression: prog || null
+                };
+            })
+
+// console.log("step3 good")
+// console.log("progression", hiraAllWithProgress[0].progression.nbViews)
+
+            let moy = hiraAllWithProgress.reduce((acc, value) => value.progression.nbViews + acc,0)/hiraAllWithProgress.length
+
+            let filtered = hiraAllWithProgress.filter(e =>  { return e.progression.nbViews > 0})
+            filtered && filtered.length > nbHira ? null : filtered = hiraAllWithProgress.filter(e =>  { return e.progression.nbViews >= moy})
+
+
+// console.log(hiraAllWithProgress)
+// console.log(moy)
+// console.log("step4 good")
+// console.log("filtered", filtered.length)
+
+
+            //  
+            let selected = [];
+
+            for (let i = 0 ; i < nbHira; i++){
+
+                if(filtered.length > 0){
+
+                    const randomIndex = Math.floor(Math.random() * filtered.length);
+
+                    selected.push(filtered[randomIndex])
+                    filtered.splice(randomIndex, 1)
+                } else {
+
+                    const randomIndex = Math.floor(Math.random() * hiraAllWithProgress.length);
+
+                    selected.push(hiraAllWithProgress[randomIndex])
+                    hiraAllWithProgress.splice(randomIndex, 1)
+                }
+            }
+
+
+// console.log("step5 good")
+// console.log("selected", selected.length)
+
+            return res.json(selected)
+
+        } catch(error) {
+            
+            return res.json({error: error, result: false})
+        }
     }
-
-
-
+    
 })
 
-router.post("/OnlyViewedCard", async (req, res) => {
+router.post("/Viewed&Unviewed", async (req, res) =>{
 
-                            // ajout token plus tard
+    // ajout token plus tard
     if (!checkBody(req.body, ["id", "password"])) {
         res.json({ result: false, error: "Missing or empty fields" });
         return;
@@ -110,10 +344,10 @@ router.post("/OnlyViewedCard", async (req, res) => {
     const NewBatchKata = req.body.needNewBatchKata
     const NewBatchHira = req.body.needNewBatchHira
     
-    const onlyViewedKata = req.body.onlyViewedKata
-    const onlyViewedHira = req.body.onlyViewedHira
 
-    if (req.body.NbByCursorKata && !isNaN(nbKata)) {
+
+    // Kata
+    if (req.body.NbByCursorKata && !isNaN(nbKata) && NewBatchKata) {
         try {
             let user = await User.findById({_id: req.body.id})
             .populate("katakanaProgress")
@@ -147,7 +381,9 @@ router.post("/OnlyViewedCard", async (req, res) => {
             return res.json(error)
         }}
 
-    else if (req.body.NbByCursorHira && !isNaN(nbHira)) {
+
+        // Hira
+    else if (req.body.NbByCursorHira && !isNaN(nbHira) && NewBatchHira) {
         try {
 
             let user = await User.findById({_id: req.body.id})
@@ -179,12 +415,6 @@ router.post("/OnlyViewedCard", async (req, res) => {
     }}
 
     res.json({result: false})
-
-
-})
-
-router.post("/Viewed&Unviewed", async (req, res) =>{
-    
 })
 
 
