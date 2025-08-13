92573273e33c5b74b793504f9d9853f735d6bf4c
canadas hugo <canadashcb@gmail.com>
2025-08-13 14:01:22 +0200

    maj BD

--- models/Hiragana.js
+++ models/Hiragana.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const hiraganaSchema = new mongoose.Schema({
     name: String,
+    type : String,
+    number : String
 });
 
 const Hiragana = mongoose.model('Hiragana', hiraganaSchema);
--- route/index.js
+++ route/index.js
@@ -5,37 +5,92 @@ const Hiragana = require("../models/Hiragana");
 const uid2 = require("uid2");
 
 
+const hiraganaNames = [
+  "01-hiragana-a", "02-hiragana-i", "03-hiragana-u", "04-hiragana-e", "05-hiragana-o",
+  "06-hiragana-ka", "07-hiragana-ki", "08-hiragana-ku", "09-hiragana-ke", "10-hiragana-ko",
+  "11-hiragana-sa", "12-hiragana-shi", "13-hiragana-su", "14-hiragana-se", "15-hiragana-so",
+  "16-hiragana-ta", "17-hiragana-chi", "18-hiragana-tsu", "19-hiragana-te", "20-hiragana-to",
+  "21-hiragana-na", "22-hiragana-ni", "23-hiragana-nu", "24-hiragana-ne", "25-hiragana-no",
+  "26-hiragana-ha", "27-hiragana-hi", "28-hiragana-fu", "29-hiragana-he", "30-hiragana-ho",
+  "31-hiragana-ma", "32-hiragana-mi", "33-hiragana-mu", "34-hiragana-me", "35-hiragana-mo",
+  "36-hiragana-ya", "38-hiragana-yu", "40-hiragana-yo",
+  "41-hiragana-ra", "42-hiragana-ri", "43-hiragana-ru", "44-hiragana-re", "45-hiragana-ro",
+  "46-hiragana-wa", "50-hiragana-wo", "51-hiragana-n"
+];
+
+const katakanaNames = [
+  "01-katakana-a", "02-katakana-i", "03-katakana-u", "04-katakana-e", "05-katakana-o",
+  "06-katakana-ka", "07-katakana-ki", "08-katakana-ku", "09-katakana-ke", "10-katakana-ko",
+  "11-katakana-sa", "12-katakana-shi", "13-katakana-su", "14-katakana-se", "15-katakana-so",
+  "16-katakana-ta", "17-katakana-chi", "18-katakana-tsu", "19-katakana-te", "20-katakana-to",
+  "21-katakana-na", "22-katakana-ni", "23-katakana-nu", "24-katakana-ne", "25-katakana-no",
+  "26-katakana-ha", "27-katakana-hi", "28-katakana-fu", "29-katakana-he", "30-katakana-ho",
+  "31-katakana-ma", "32-katakana-mi", "33-katakana-mu", "34-katakana-me", "35-katakana-mo",
+  "36-katakana-ya", "38-katakana-yu", "40-katakana-yo",
+  "41-katakana-ra", "42-katakana-ri", "43-katakana-ru", "44-katakana-re", "45-katakana-ro",
+  "46-katakana-wa", "50-katakana-wo", "51-katakana-n"
+];
+
 const kataNames = [
-  'a', 'i', 'u', 'e', 'o',
-  'ka', 'ki', 'ku', 'ke', 'ko',
-  'sa', 'shi', 'su', 'se', 'so',
-  'ta', 'chi', 'tsu', 'te', 'to',
-  'na', 'ni', 'nu', 'ne', 'no',
-  'ha', 'hi', 'fu', 'he', 'ho',
-  'ma', 'mi', 'mu', 'me', 'mo',
-  'ya', 'yu', 'yo',
-  'ra', 'ri', 'ru', 're', 'ro',
-  'wa', 'wo', 'n'
+'a', 'i', 'u', 'e', 'o',
+'ka', 'ki', 'ku', 'ke', 'ko',
+'sa', 'shi', 'su', 'se', 'so',
+'ta', 'chi', 'tsu', 'te', 'to',
+'na', 'ni', 'nu', 'ne', 'no',
+'ha', 'hi', 'fu', 'he', 'ho',
+'ma', 'mi', 'mu', 'me', 'mo',
+'ya', 'yu', 'yo',
+'ra', 'ri', 'ru', 're', 'ro',
+'wa', 'wo', 'n'
 ];
 
 
-
 // créer la liste des kata en bd, commande en dev pas en prod
-router.post("/kataGen", async (req, res) => {
+router.post("/kanaGen", async (req, res) => {
   try {
-    for (let i = 0; i < kataNames.length; i++) {
-      const kata = await new Katakana({
-        name: kataNames[i],
+    for (let i = 0; i < hiraganaNames.length; i++) {
+
+      const [numK, typeK, nameK] = katakanaNames[i].split("-");
+      const [numH, typeH, nameH] = hiraganaNames[i].split("-");
+
+      await new Katakana({
+        name: nameK[i],
+        number: numK,     
+        type: typeK,        
+        fullName: katakanaNames[i] 
       }).save();
-      const hira = await new Hiragana({
-        name: kataNames[i],
-  
+
+      await new Hiragana({
+        name: nameH[i],
+        number: numH,
+        type: typeH,
+        fullName: hiraganaNames[i]
       }).save();
     }
+
     res.json({ result: true });
-    
+
   } catch (error) {
-    res.json({error : error, message: "erreur d'enregistrement des katas"})
+    res.json({ error, message: "erreur d'enregistrement des katas" });
+  }
+});
+
+
+router.post('/majKana', async (req, res) => {
+  try {
+    for (let i = 0; i < kataNames.length; i++) {
+
+      const [numK, typeK, nameK] = katakanaNames[i].split('-');
+      const [numH, typeH, nameH] = hiraganaNames[i].split('-');
+
+      await Katakana.updateOne({ name: kataNames[i] }, { type: typeK, number: numK });
+
+      await Hiragana.updateOne({ name: kataNames[i] }, { type: typeH, number: numH });
+    }
+
+    res.json({ result: true });
+  } catch (err) {
+    res.json({ error: err });
   }
 });
 
