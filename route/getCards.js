f512771fd9bcc7131a41449a50e6d31f7cd52081
canadas hugo <canadashcb@gmail.com>
2025-08-15 11:44:04 +0200

    m

--- route/getCards.js
+++ route/getCards.js
@@ -11,72 +11,32 @@ const { checkBody } = require("../modules/checkBody");
 
 // fonction pour filtrer les cartes en fonction du type de filtre et de leurs priorités
 function filter(filterType, list, nbSlider) {
-    console.log('filter début');
 
-    let katakanaLimite = 10;
-    let hiraganaLimite = 45 + 10;
-
-    // Katakana
-    let value = 0;
-    for (let i = 0; i <= katakanaLimite && i < 46; i++) {
-        value += list[i]?.priority || 0; 
-        console.log(value)
-    }
-    if (value < katakanaLimite/1.25) {
-        katakanaLimite += 5;
-        value = 0
-    }
-    
-    // Hiragana
-    
-    value = 0;
-    for (let i = 46; i <= hiraganaLimite && i < 92; i++) {
-        value += list[i]?.priority || 0; 
-    }
-    if (value < hiraganaLimite/1.25) {
-        hiraganaLimite += 5
-        value = 0
-    };
-
-    console.log("limites établies", { katakanaLimite, hiraganaLimite });
-
-    let filtered = [
-        ...list.filter((_, index) => index < katakanaLimite),
-        ...list.filter((_, index) => index > 45 && index < hiraganaLimite)
-    ];
-
-    let averageView = filtered.reduce((acc, v) => acc + v.nbViews, 0) / list.length;
-
-    const keepMinSize = (arr, extraFilter) => {
-        if (arr.length < nbSlider) {
-            return [
-                ...arr,
-                ...list.filter(extraFilter)
-            ];
-        }
-        return arr;
-    };
+    let averageView = list.reduce((acc, value) => value.nbViews + acc, 0) / list.length
+    let filtered;
 
     if (filterType === "neverViewed") {
-        filtered = [...filtered.filter((e, i) =>
-            e.nbViews <= averageView || e.priority >= Math.random() || e.isFavorite
-        ), ...filtered]
-        return filtered;
-     }
 
-    if (filterType === "onlyViewed" || filterType === "ChallengeAll") {
-        filtered = filtered.filter(e => e.nbViews > 0 || e.isFavorite);
-        filtered = [...filtered.filter(e => e.nbViews > averageView || e.isFavorite), ...filtered]
-        filtered = [...filtered.filter(e => e.priority >= Math.random() || e.isFavorite), ...filterType];
-        return filtered;
+        filtered = list.filter(e => { return e.nbViews <= 0 })
+        filtered && filtered.length > nbSlider ? null : filtered = list.filter(e => { return (e.nbViews < averageView && e.priority >= Math.random()) || e.isFavorite })
+
+        return filtered.length
     }
 
-    if (filterType === "all") {
-        filtered = filtered.filter(e => e.priority >= Math.random() || e.isFavorite);
-        return filtered;
+    else if (filterType === "onlyViewed" || filterType === "ChallengeAll") {
+        filtered = list.filter(e => { return e.nbViews > 0 || e.isFavorite})
+        filtered && filtered.length > nbSlider ? null : filtered = list.filter(e => { return e.nbViews > averageView || e.isFavorite })
+        filtered = filtered.filter(e => { return e.priority >= Math.random() || e.isFavorite})
+
+        return filtered
+    }
+
+    else if (filterType === "all") {
+        filtered = list.filter(e => { return ( e.priority >= Math.random()) || e.isFavorite})
+
+        return filtered
     }
 }
-
 
 
 
@@ -84,15 +44,15 @@ router.post("/getCards", async (req, res) => {
 
 
 
-    const { nbSlider, kataType, filterType, id, isDevMode, token } = req.body
-
+    const { nbSlider, kataType, filterType, id, isDevMode, token} = req.body
+     
     console.log("1", id, token)
     // ajout token plus tard
     if (!checkBody(req.body, ["id", "token"])) {
-        res.json({ result: false, message: "Champs manquants ou vides", error: "Missing or empty fields" });
+        res.json({ result: false, message: "Champs manquants ou vides" , error: "Missing or empty fields" });
         return;
     }
-
+    
     console.log("2")
     // Unviewed Kata Ok
     try {
@@ -100,30 +60,30 @@ router.post("/getCards", async (req, res) => {
 
         // on charge user et on populate les kataProgress selon la demande, qu'on populate a leurs tours des kata correspondants
         let user = await User.findById({ _id: id })
-        kataType === "hiragana" || kataType === "all" ? await user.populate({
-            path: "hiraganaProgress",
-            populate: {
-                path: "hiraganaId",
-                model: "Hiragana"
-            }
-        }) : null
-        kataType === "katakana" || kataType === "all" ? await user.populate({
-            path: "katakanaProgress",
-            populate: {
-                path: 'katakanaId',
-                model: 'Katakana'
-            }
-        }) : null
+            kataType === "hiragana" || kataType === "all" ? await user.populate({
+                path: "hiraganaProgress",
+                populate: {
+                    path: "hiraganaId",
+                    model: "Hiragana"
+                }
+            }) : null
+            kataType === "katakana" || kataType === "all" ? await user.populate({
+                path: "katakanaProgress",
+                populate: {
+                    path: 'katakanaId',
+                    model: 'Katakana'
+                }
+            }) : null
 
-        console.log("3", user._id)
+console.log("3", user._id)
 
         // verification du token
-        if (user.token !== token) {
-            res.json({ result: false, message: "token invalide", error: "token invalide" });
+         if (user.token !== token) {
+            res.json({ result: false, message: "token invalide" ,error: "token invalide" });
             return
         }
 
-        console.log("4")
+console.log("4")
 
         // creation de la liste non filtrée
         let list = [];
@@ -141,7 +101,7 @@ router.post("/getCards", async (req, res) => {
         //  selection des cartes
         let selected = [];
 
-        console.log("5")
+console.log("5")
 
         for (let i = 0; i < nbSlider; i++) {
             if (filtered.length > 0) {
@@ -155,14 +115,14 @@ router.post("/getCards", async (req, res) => {
                 list.splice(randomIndex, 1)
             }
         }
-
-        console.log("6")
+        
+console.log("6")
 
         // on retourne la liste des kata selected sans leurs progression
-        return res.json({ result: true, data: isDevMode ? selected : selected.map(select => select.hiraganaId || select.katakanaId) })
+        return res.json({ result: true, data: isDevMode ? selected : selected.map(select => select.hiraganaId || select.katakanaId )})
 
     } catch (error) {
-        return res.json({ result: false, message: "erreur lors de la récuperation des kata", error: error })
+        return res.json({result : false , message : "erreur lors de la récuperation des kata" , error : error})
     }
 })
 
